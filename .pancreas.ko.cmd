2b1f9c0ca12cd39b849170efb3c327c4269d1ff6
guilherme.andrade <guilherme.andrade@gmail.com>
2019-02-27 09:25:11 -0300

    Add more files to gitignore
    Add more files to gitignore

--- .pancreas.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/guilherme/eletronica/driver/pancreas/pancreas.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/guilherme/eletronica/driver/pancreas/pancreas.ko /home/guilherme/eletronica/driver/pancreas/pancreas.o /home/guilherme/eletronica/driver/pancreas/pancreas.mod.o ;  true
